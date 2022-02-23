1c858e05b96b2dec30632d5afd12f6e6e70673f1
Adrian Ramirez Mattey <aramirezmattey@gmail.com>
2022-02-23 10:08:33 -0600

    empezar tripletas

---
+++ tripletas.cpp
@@ -0,0 +1,11 @@
+
+// todo corre sobre la función principal para efectos de simplicidad
+void main() {
+
+    // tripletas de ejemplo
+    int ana[] = {10,32,14};
+    int bob[] = {5, 34, 17};
+    // arreglo con los valores del restultado
+    // ordenado en el orden: resAna y resBob
+    int res[] = {0,0};
+}
